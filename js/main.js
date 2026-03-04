ef33e4c413a3313c5268eebcce37d8f1aab78336
Lourdes Nguyen <nguyenlo@cua.edu>
2026-03-04 17:52:16 -0500

    class exercise with javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
 console.log("Hello World!");
+function fullname(firstname, lastname) {
+    return firstname + " " +lastname;
+}
+
+const res = fullname("Lourdes", "Nguyen");
+console.log(res);
+
+function avg(...args) {
+    let sum = 0;
+    for (const item of args) {
+        sum += item;
+    }
+    return sum / args.length;
+}
+
+const avgres = avg(2, 3, 4, 5)
+console.log(avgres);
+
+const arrayOfNotes = {};
+
+const add = function(text, priority) {
+    arrayOfNotes.push({
+        text,
+        priority, //low,medium,high
+        author: "Lourdes"
+    });
+};
+
+const list = function() {
+    arrayOfNotes.forEach(item => {
+        console.log(item);
+    })
+
+};
+
+const remove = function(index) {
+
+};
